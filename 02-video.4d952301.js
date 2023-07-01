5541ab62ff1c8c35a4ab4ede392ba7b109b69b7a
chaban-dmytro <84576171+chaban-dmytro@users.noreply.github.com>
2023-07-01 11:02:48 +0000

    Deploying to gh-pages from @ chaban-dmytro/goit-js-hw-08@659c9085450a81f9c5d3c0a931a57b9db43855e0 🚀

---
+++ 02-video.4d952301.js
@@ -0,0 +1,2 @@
+console.log(localStorage),localStorage.setItem("name","Dima"),console.log(localStorage.getItem("name"));
+//# sourceMappingURL=02-video.4d952301.js.map
---
+++ 02-video.fce7c1f7.js
@@ -0,0 +1,2 @@
+console.log(localStorage),localStorage.setItem("name","Dima"),console.log(localStorage.getItem("name"));
+//# sourceMappingURL=02-video.fce7c1f7.js.map
