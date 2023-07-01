64bf8520356a121b4750404e5992300a71953ee4
chaban-dmytro <dimach@ukr.net>
2023-07-01 14:04:58 +0200

    finish task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,14 @@
+import Player from '@vimeo/player';
 
-console.log( localStorage );
+const throttle = require( 'lodash.throttle' );
+const iframe = document.querySelector( 'iframe' );
+const player = new Player(iframe);
 
-localStorage.setItem( "name", "Dima" );
+player.setCurrentTime( localStorage.getItem('videoplayer-current-time') ?? 0);
 
-console.log(localStorage.getItem( 'name' ));
+player.on( 'timeupdate', throttle(checkTime, 1000));
+
+function checkTime( event ) {
+  localStorage.setItem("videoplayer-current-time", JSON.stringify(event.seconds))
+  console.log(event.seconds);
+}
