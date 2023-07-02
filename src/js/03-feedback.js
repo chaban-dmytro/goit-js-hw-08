43b08a5dd928ef426eaffd18035d0cfb30dfb3f7
chaban-dmytro <dimach@ukr.net>
2023-07-02 11:16:24 +0200

    fix bugs

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,25 +1,29 @@
+const throttle = require( 'lodash.throttle' );
 const formEl = document.querySelector( '.feedback-form' );
-const formInputEl = document.querySelector( '.feedback-form input' )
-const formTextareaEl = document.querySelector( '.feedback-form textarea' )
+const formInputEl = document.querySelector( '.feedback-form input' );
+const formTextareaEl = document.querySelector( '.feedback-form textarea' );
+const formSubmitBtnEl = document.querySelector( '.feedback-form button' );
 
-formEl.addEventListener( 'input', onInput );
+formEl.addEventListener( 'input', throttle(onInput, 500) );
+formSubmitBtnEl.addEventListener( 'click', submitForm );
+// якщо я пишу подію 'submit' заміть 'click' то все що знаходиться у функції submitForm чомусь не спрацьовує
+const obj = {}
 
 setInputFromLocaleStorage();
 
+function isLocalStorageEmpty() {
+  return (localStorage.getItem('feedback-form-state') === null);
+}
+
 function setInputFromLocaleStorage() {
   if ( !isLocalStorageEmpty() ) {
-    // console.log( 'not empty' );
     const parseObj = JSON.parse(localStorage.getItem('feedback-form-state'))
     formInputEl.value = parseObj.email ?? null;
     formTextareaEl.value = parseObj.message ?? null;
+    obj.email = parseObj.email;
+    obj.message = parseObj.message;
   } 
 }
-
-function isLocalStorageEmpty() {
-  return ( localStorage.length === 0 );
-}
-
-const obj = {}
 
 function onInput( event ) {
   if ( event.target.name === 'email' ) {
@@ -29,3 +33,12 @@ function onInput( event ) {
   }
   localStorage.setItem("feedback-form-state", JSON.stringify(obj));
 }
+
+function submitForm ( event ) {
+  event.preventDefault();
+  console.log(obj);
+  localStorage.removeItem( 'feedback-form-state' );
+  formInputEl.value = '';
+  formTextareaEl.value = '';
+}
+
